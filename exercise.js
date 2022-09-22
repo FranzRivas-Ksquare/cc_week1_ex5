e510c8be0a7435d03371a57b0c3d73acd6507a26
FranzRivas-Ksquare <franz.rivas@itksquare.edu.mx>
2022-09-22 17:52:21 -0500

    finish activity 5

--- exercise.js
+++ exercise.js
@@ -5,17 +5,32 @@
 
     Must return the array with only a single level
 
-
+// function faltten:
+// description: flatt a nested array
+// input: array
+// output: retrun 1D array
 
 */
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const flatten = (arr) => {
     /* Only make changes below this comment */
-    
+    const result = [];
+    const recursiveFlat = (nestArr) => {
+        let counter = 0
+        while (counter < nestArr.length) {
+        const value = nestArr[counter];
+        if (Array.isArray(value)) {
+            recursiveFlat(value);
+        } else {
+            result.push(value)
+        }
+        counter++;
+        }
+    }
+    recursiveFlat(arr);
+    return result;
     /* Only make changes below this comment */
 }
-
-
 
 
 
